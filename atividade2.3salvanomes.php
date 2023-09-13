1cdcf956f8cd59d31ec310f0da1cbaee3c0a0e44
Jotanimal <114446872+Jotanimal@users.noreply.github.com>
2023-09-13 08:46:07 -0300

    Update atividade2.3salvanomes.php

--- atividade2.3salvanomes.php
+++ atividade2.3salvanomes.php
@@ -9,16 +9,19 @@
     $arquivo;
     $nomes;
 
-    $arquivo = fopen("nomes.txt", "w");
-    if ($arquivo) {
-        fwrite($arquivo, $nome1 . "\n");
-        fwrite($arquivo, $nome2 . "\n");
-        fwrite($arquivo, $nome3 . "\n");
-        fwrite($arquivo, $nome4 . "\n");
-        fwrite($arquivo, $nome5 . "\n");
+    // Função para criar e escrever arquivo
+    function criaArquivo($n1, $n2, $n3, $n4, $n5){
+        $arquivo = fopen("nomes.txt", "w");
+        if ($arquivo) {
+            fwrite($arquivo, $n1 . "\n");
+            fwrite($arquivo, $n2 . "\n");
+            fwrite($arquivo, $n3 . "\n");
+            fwrite($arquivo, $n4 . "\n");
+            fwrite($arquivo, $n5 . "\n");
 
-        fclose($arquivo);
-
+            fclose($arquivo);
+        }
+        
         $nomes = file_get_contents("nomes.txt");
         
         echo "Conteúdo do arquivo: <br>" . $nomes;
