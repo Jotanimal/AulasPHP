63b5037694ce599d32bfe3c1aef2c777a9deb40a
Jotanimal <114446872+Jotanimal@users.noreply.github.com>
2023-09-13 08:23:29 -0300

    Update atividade2.2leArquivo.php

--- atividade2.2leArquivo.php
+++ atividade2.2leArquivo.php
@@ -3,12 +3,14 @@
     $arquivo;
     $leFrases;
 
+        // Abrindo arquivo para leitura
     $arquivo = fopen("frases.txt", "r");
     if($arquivo){
+        // Leitura do arquivo - todo de uma vez
         $leFrases = file_get_contents("frases.txt");
         echo "Conteúdo do arquivo criado: " . $leFrases;
     }else{
         echo "Erro ao abrir o arquivo: Arquivo invalido";
     }
-
+        // Leitura do arquivo - linha por linha #echo fgets($arquivo);
 ?>
