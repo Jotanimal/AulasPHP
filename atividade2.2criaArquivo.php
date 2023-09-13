3871b306678e50c343e7f8ecc42aab9dc1f24eec
Jotanimal <114446872+Jotanimal@users.noreply.github.com>
2023-09-13 08:20:58 -0300

    Update atividade2.2criaArquivo.php

--- atividade2.2criaArquivo.php
+++ atividade2.2criaArquivo.php
@@ -1,12 +1,19 @@
 <?php
     $arquivo;
 
+        // Manipulação de arquivos
+        // W = Abrir para ESCRITA
+        // R = Abrir para LEITURA
+        // A = Abrir para ADICIONAR
+
     $arquivo = fopen("frases.txt", "w");
         if($arquivo){
+            // Escrevendo no arquivo
             fwrite($arquivo, "Eu gosto de comer batata \n");
             fwrite($arquivo, "Baldurs Gate 4 \n");
             fwrite($arquivo, "Tiago Iorc nunca foi legal \n");
 
+            // Fechando arquivo
             fclose($arquivo);
 
             echo "Arquivo criado com sucesso";
