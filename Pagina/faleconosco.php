eda20cf24dc45cb2b7361a4e76fa0c930e6f6ebf
Jotanimal <114446872+Jotanimal@users.noreply.github.com>
2023-09-19 21:39:25 -0300

    Update faleconosco.php

--- Pagina/faleconosco.php
+++ Pagina/faleconosco.php
@@ -1,3 +1,9 @@
 <h1>CONTATO </h1>
 
-<p>Entre em contato conosco por meio de... </p>
+<p>Entre em contato conosco por meio desse formulario: </p>
+<form method="post">
+    Nome: <input type="text" name="nome"><br>
+    Telefone: <input type="number" name="tel"><br>
+    Mensagem: <input type="text" name="mensagem"><br>
+    <input type="submit" value="Enviar mensagem">
+</form>
